cb5a87d92f5bffacd7e70a1f20a8ab0a573fd88c
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-21 21:27:01 +0900

    update img

--- JavaScript/background.js
+++ JavaScript/background.js
@@ -5,7 +5,7 @@ const IMG_NUM = 3;
 function displayImage() {
     const randomNumber = Math.floor(Math.random() * IMG_NUM);
     const background_img = new Image();
-    background_img.src = `img/${randomNumber + 1}.jpg`;
+    background_img.src = `img/${randomNumber + 1}.PNG`;
     background_img.classList.add("bgimage");
     body.appendChild(background_img);
 }
