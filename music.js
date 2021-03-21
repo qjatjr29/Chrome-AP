f895e55e891d421cfda360dd95b9c48a0475f452
qjatjr29 <74031333+qjatjr29@users.noreply.github.com>
2021-03-21 09:15:08 +0900

    Add BGM

---
+++ music.js
@@ -0,0 +1,52 @@
+const BGM_button = document.querySelector(".BGM_button");
+const BGM_NUMBER = 4;
+var audio = new Audio();
+
+
+function playBgm() {
+    const randomNum = Math.floor(Math.random() * BGM_NUMBER);
+    // event.preventDefault();
+    console.log("o")
+    console.log(randomNum);
+    audio.src = `music/${randomNum + 1}.mp3`;
+    audio.play();
+    BGM_button.classList.add("playing");
+    BGM_button.innerHTML = "PAUSE";
+    audio.addEventListener("ended", function () {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+}
+function pauseBgm() {
+    console.log("x")
+    audio.pause();
+    BGM_button.innerHTML = "BGM";
+    BGM_button.classList.remove("playing");
+}
+
+function checkPlaying(event) {
+    event.preventDefault();
+    console.log(BGM_button.classList.contains("playing"));
+    if (BGM_button.classList.contains("playing")) {
+        console.log("x")
+        pauseBgm();
+    }
+    else {
+        playBgm();
+    }
+}
+
+function init() {
+    // if (BGM_button.classList.contains("playing") == true) {
+
+    //     BGM_button.addEventListener("click", pauseBgm);
+    // }
+    // else {
+    //     console.log("x")
+    //     BGM_button.addEventListener("click", playBgm);
+    // }
+    BGM_button.addEventListener("click", checkPlaying);
+
+}
+
+init();
